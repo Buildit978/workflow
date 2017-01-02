949e570871934603dfaaae476ccdad2481cb5d9f
Keith Evanson <keithevanson@keith-evansons-macbook-pro.local>
2017-01-02 09:42:25 -0500

    concat js script

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp'),
     gutil =  require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee')
+	concat = require("gulp-concat");
 	
 	var coffeeSource = ['components/coffee/tagline.coffee']
+	var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgird.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js',]
 
 gulp.task('coffee', function(){
 	gulp.src('coffeeSource')
@@ -10,4 +16,10 @@ gulp.task('coffee', function(){
 		.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'))
 	
+});
+
+gulp.task('js',function(){
+	gulp.src('jsSource')
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
