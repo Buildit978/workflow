0a1bcda71fc21115e11f95745983220b423b0b98
Keith Evanson <keithevanson@keith-evansons-macbook-pro.local>
2017-01-09 07:37:58 -0500

    Added Jsonminify need to fix production css

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
 	minifyHTML = require('gulp-minify-html'),
+	jsonminify = require('gulp-jsonminify'),
 	compass = require('gulp-compass'),
     browserify = require('gulp-browserify');
 	
@@ -75,7 +76,7 @@ gulp.task('watch', function() {
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
   gulp.watch('builds/development/*.html', ['html']);
-  gulp.watch(jsonSources, ['json']);
+  gulp.watch('builds/development/js/*.json',['json']);
 });
 
 gulp.task('connect', function(){
@@ -87,7 +88,9 @@ gulp.task('connect', function(){
 
 
 gulp.task('json', function(){
-	gulp.src(outputDir + 'js/*.json')
+	gulp.src('builds/development/js/*.json')
+	.pipe(gulpif(env === 'production', jsonminify()))
+	.pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
 	.pipe(connect.reload())
 });
 
